66efaaece532684cf5e30ce797af48585cf413fa
menjaw <menjaholm@gmail.com>
2017-11-01 18:21:16 +0100

    Facade modified

--- seedServer/src/main/java/facades/PlaceFacade.java
+++ seedServer/src/main/java/facades/PlaceFacade.java
@@ -16,25 +16,33 @@ import javax.persistence.Persistence;
  * @author Menja
  */
 public class PlaceFacade {
-      EntityManagerFactory emf;
 
-  public PlaceFacade(EntityManagerFactory emf) {
-    this.emf = emf;   
-  }
+    EntityManagerFactory emf;
 
-  private EntityManager getEntityManager() {
-    return emf.createEntityManager();
-  }
-  public List<Place> getAllPlaces(){
-      EntityManager em = emf.createEntityManager();
-      List<Place> places = em.createNamedQuery("Place.findAll").getResultList();
-      return places;
-  }
-//  public Place getPlaceById(){}
-//  public Place getPlaceByCity(){}
+    //Constructor
+    public PlaceFacade(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
     public static void main(String[] args) {
+        //Creates a persistence unit and new instance of Placefacade
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu_development");
         PlaceFacade pf = new PlaceFacade(emf);
+
+        /*Runs the different methods*/
         System.out.println(pf.getAllPlaces());
     }
+
+    //The methods
+    public List<Place> getAllPlaces() {
+        EntityManager em = emf.createEntityManager();
+        List<Place> places = em.createNamedQuery("Place.findAll").getResultList();
+        return places;
+    }
+//  public Place getPlaceById(){}
+//  public Place getPlaceByCity(){}
 }
