560ece050f362de496b5302653af8b67f50cbcdc
menjaw <menjaholm@gmail.com>
2017-11-03 12:18:52 +0100

    rest facade and rest done for places 3 methods works

--- seedServer/src/main/java/rest/All.java
+++ seedServer/src/main/java/rest/All.java
@@ -6,12 +6,14 @@ import facades.PlaceFacade;
 import interfaces.IPlaceFacade;
 import java.util.List;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import security.IUserFacade;
@@ -26,8 +28,8 @@ import security.UserFacadeFactory;
 public class All {
 
     IUserFacade userFacade;
-    EntityManagerFactory emf;
-    
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu_development");
+
     private final IPlaceFacade placeFacade = new PlaceFacade(emf);
     private final JsonConverter jsonConverter = new JsonConverter();
 
@@ -41,7 +43,7 @@ public class All {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getText() {
-        return " {\"message\" : \"result for all\"}";
+        return " {\"message\" : \"IT WORKS! result for all\"}";
     }
 
     @GET
@@ -49,6 +51,22 @@ public class All {
     @Path("/places")
     public String getAllPlaces() {
         List<Place> places = placeFacade.getAllPlaces();
+        return jsonConverter.getJSONFromPlaces(places);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/place/{id}")
+    public String getPlaceById(@PathParam("id") int id) {
+        Place place = placeFacade.getPlaceById(id);
+        return jsonConverter.getJSONFromPlace(place);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/place/city/{city}")
+    public String getPlaceByCity(@PathParam("city") String city) {
+        List<Place> places = placeFacade.getPlaceByCity(city);
         return jsonConverter.getJSONFromPlaces(places);
     }
 
