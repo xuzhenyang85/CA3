a3c0073dc1f4e01700317df9be169094df8b9444
xuzhenyang85 <cph-xy171@cphbusiness.dk>
2017-11-05 01:56:47 +0100

    client seed place.js

--- seedClient/src/facades/placeFacade.js
+++ seedClient/src/facades/placeFacade.js
@@ -1,16 +1,36 @@
+import fetchHelper, {errorChecker} from "./fetchHelpers"
+const URL = require("../../package.json").serverURL;
 /**
  * Created by Xu on 02-11-2017.
  */
- class PlaceFacade {
-     getData(Search, cb) {
-
-         fetch("https://xudk.me/backend/ca3/api/all/place/" + Search).then(res => res.json()).then(data => {
-             console.log(data)
-             cb(data);
+ class PlaceStore {
+   constructor() {
+     this._data = "";
+     this._errorMessage = "";
+   }
+   getPlaces = (cb , path) => {
+       this._errorMessage = "";
+       this._messageFromServer = "";
+       let resFromFirstPromise=null;  //Pass on response the "second" promise so we can read errors from server
+       const options = fetchHelper.makeOptions("GET", true);
+       fetch(URL + path, options)
+         .then((res) => {
+           resFromFirstPromise = res;
+           return res.json();
+         }).then((data) => {
+           errorChecker(resFromFirstPromise,data);
+           if (cb) {
+             cb(null, data.users)
+           }
+         }).catch(err => {
+           if (cb) {
+             cb({ err: fetchHelper.addJustErrorMessage(err) })
+           }
          })
-
-
      }
  }
+let placeStore = new PlaceStore();
 
- export default new PlaceFacade();
+//Only for debugging
+//window.userStore = userStore;
+export default placeStore;
--- seedClient/src/pages/Place.js
+++ seedClient/src/pages/Place.js
@@ -1,57 +1,45 @@
 /**
  * Created by Xu on 01-11-2017.
  */
- import React, { Component } from 'react';
- import placeFacade from '../facades/placeFacade';
+import React, {Component} from 'react'
+import placeData from "../facades/placeFacade";
 
- class Search extends Component {
+class PlaceList extends Component {
+  constructor() {
+  super();
+  this.state = { message: "", data: "", err: "" }
+}
 
-     constructor() {
-         super();
-         this.state = { places: { search: "" }, data: [] }
-     }
+componentWillMount() {
+  /*
+  This will fetch data each time you navigate to this route
+  If only required once, add "logic" to determine when data should be "refetched"
+  */
+  placeData.getPlaces((e, data) => {
+    if (e) {
+      return this.setState({ err: e.err })
+    }
+    var message ="";
+    message = e;
+    console.log(data);
+  }, "api/all/places");
+}
 
+render() {
+  return (
+    <div>
+      <h2>Sommerhus</h2>
+      <p>This list contains all the places in the system</p>
+      <img src="https://xudk.me/backend/ca3/img/image.jpg"  />
+      {this.state.message}
+      {this.state.err && (
+        <div className="alert alert-danger errmsg-left" role="alert">
+          {this.state.err}
+        </div>
+      )}
+    </div>
+  )
+}
+}
 
-     onChange = (e) => {
-         const propertyName = e.target.id;
-         const value = e.target.value;
-         let places = this.state.places;
-         places[propertyName] = value;
-         this.setState({ places });
-     }
-     componentWillMount() {
-         /*
-         This will fetch data each time you navigate to this route
-         If only required once, add "logic" to determine when data should be "refetched"
-         */
-     }
-
-     render() {
-         const rows = this.state.data.map(row => {
-             return (
-                 <tr key={row.id}>
-                     <td>{row.id}</td>
-                     <td>{row.street}</td>
-                     <td>{row.zip}</td>
-                     <td>{row.city}</td>
-                     <td>{row.geolat}</td>
-                     <td>{row.geolng}</td>
-                     <td>{row.description}</td>
-                     <td>{row.rating}</td>
-                 </tr>
-             )
-         })
-         return (
-             <div>
-                 <input type="text" value={this.state.places.search} onChange={this.onChange} id="search" placeholder="Search Lyngby..." />
-                 <button type="button" onClick={this.handleClick}>search</button>
-
-                 <div>
-                     <table className="table table-striped"> <thead ><tr><th>Id</th><th>Street</th><th>Zip</th><th>City</th><th>Geolat</th><th>Geolng</th><th>Description</th><th>Rating</th></tr></thead><tbody>{rows} </tbody> </table>
-                 </div>
-             </div>
-         )
-     }
- }
-
- export default Search;
+export default PlaceList;
